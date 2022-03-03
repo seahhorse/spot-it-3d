df887c2d05047b7256a955b5d0f4cdef1e00fad8
Lau Yan Han <sps08.lauyanhan@gmail.com>
2022-03-03 11:18:56 +0800

    feat: Make csv/json scripts handle 2 cameras at once

---
+++ scripts/get_mat.m
@@ -0,0 +1,35 @@
+function [matrix] = get_mat(filename)
+% Convert CSV file of detections for a given camera into a 3D matrix
+% Input: Name of CSV file
+% Output: 3D matrix with the following dimenations:
+%   Rows (1) = frame number / time
+%   Cols (2) = X (row 1) and Y (row 2) coordinates
+%   Depth (3) = Detected IDs
+
+T=readtable(filename);
+arr=table2array(T);
+
+% get list of IDs
+IDs = 0;
+count = 1;
+for i = 1:size(arr,1)
+    ID_found = find(IDs == arr(i,2));
+    if isempty(ID_found)
+        IDs(count) = arr(i,2);
+        count = count + 1;
+    end
+end
+
+matrix = zeros(2,1,size(IDs,2));
+for row = 1:size(arr,1)
+    frame = arr(row,1);
+    ID = arr(row,2);
+    x = arr(row,3);
+    y = arr(row,4);
+    ID_pos = find(IDs==ID);
+    matrix(1,frame,ID_pos) = x;
+    matrix(2,frame,ID_pos) = y;
+end
+
+end
+
--- scripts/jsontocsv.py
+++ scripts/jsontocsv.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 
 # Convert output of 2D json file to CSV for each camera
-# Inputs: input json file + camera number (e.g. 0  or 1)
-# Outputs: CSV file called "output.csv" in the same folder as this script
+# Inputs: input json file
+# Outputs: CSV files "output_<cam_number>.csv", one CSV file per camera
 # CSV file header format: [Frame Number] [ID] [centroid X] [Centroid Y] [width] [height]
 
 import json
 import csv
 import sys
+
+NUM_OF_CAMERAS = 2
 
 # Calculate XY centroid from bottom_right and top_left coordinates
 def get_centroid(bottom_right,top_left):
@@ -25,19 +27,22 @@ def get_dimensions(bottom_right,top_left):
 # Main
 ###########
 
-if len(sys.argv) != 3:
-    print("Usage: python jsontocsv.py <path to json file> <cam_number>")
+if len(sys.argv) != 2:
+    print("Usage: python jsontocsv.py <path to json file>")
     sys,exit()
 filename = sys.argv[1]
-cam = 'Cam ' + sys.argv[2]
 
 # Open json file
 with open(filename) as json_file:
     data = json.load(json_file)
 
-# create csv file
-data_file = open('output.csv','w')
-csv_writer = csv.writer(data_file)
+# create csv files, one for each camera
+data_file = [None]*NUM_OF_CAMERAS
+csv_writer = [None]*NUM_OF_CAMERAS
+for i in range(0,NUM_OF_CAMERAS):
+    csv_filename = 'output_' + str(i) + '.csv'
+    data_file[i] = open(csv_filename,'w')
+    csv_writer[i] = csv.writer(data_file[i])
 
 # add headers
 header = list()
@@ -47,27 +52,31 @@ header.append('x')
 header.append('y')
 header.append('width')
 header.append('height')
-csv_writer.writerow(header)
+for i in range(0,NUM_OF_CAMERAS):
+    csv_writer[i].writerow(header)
 
 # write data
 for frame in data:
-    # loop through detections of the specified camera in each frame
-    idx = 0
-    while idx < len(frame[cam]):
-        # for each detection, obtain centroid and ID number
-        bottom_right = eval(frame[cam][idx]['Bottom-Right']) # eval as tuple
-        top_left = eval(frame[cam][idx]['Top-Left'])
-        cen_x,cen_y = get_centroid(bottom_right,top_left)
-        width,height = get_dimensions(bottom_right,top_left)
-        # rowdata contains the data for a given detection at a given frame
-        rowdata = list()
-        rowdata.append(frame['Frame Number'])
-        rowdata.append(frame[cam][idx]['ID'])
-        rowdata.append(cen_x)
-        rowdata.append(cen_y)
-        rowdata.append(width)
-        rowdata.append(height)
-        csv_writer.writerow(rowdata)
-        idx += 1
+    for i in range(0,NUM_OF_CAMERAS):
+        # loop through detections of the specified camera in each frame
+        idx = 0
+        cam = "Cam " + str(i)
+        while idx < len(frame[cam]):
+            # for each detection, obtain centroid and ID number
+            bottom_right = eval(frame[cam][idx]['Bottom-Right']) # eval as tuple
+            top_left = eval(frame[cam][idx]['Top-Left'])
+            cen_x,cen_y = get_centroid(bottom_right,top_left)
+            width,height = get_dimensions(bottom_right,top_left)
+            # rowdata contains the data for a given detection at a given frame
+            rowdata = list()
+            rowdata.append(frame['Frame Number'])
+            rowdata.append(frame[cam][idx]['ID'])
+            rowdata.append(cen_x)
+            rowdata.append(cen_y)
+            rowdata.append(width)
+            rowdata.append(height)
+            csv_writer[i].writerow(rowdata)
+            idx += 1
 
-data_file.close()
+for i in range(0,NUM_OF_CAMERAS):
+    data_file[i].close()
